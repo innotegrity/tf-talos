ae3854a99c85017753ba2ece9180d699ce9ae64f
Josh Hogle <josh@innotegrity.com>
2025-12-17 15:36:55 -0500

    Removed unnecessary local vars

--- talos-cluster-cni/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  endpoints           = var.endpoints
-  control_plane_nodes = [for node in var.control_plane_nodes : split(".", node)[0]]
-  worker_nodes        = [for node in var.worker_nodes : split(".", node)[0]]
-}
